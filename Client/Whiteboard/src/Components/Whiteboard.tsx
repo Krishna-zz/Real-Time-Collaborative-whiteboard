29e94150a32098e849ea2498e8db9e262350142f
Krishna-zz <krishnagunjal022@gmail.com>
2025-11-02 05:41:29 +0530

    started making canvas

--- Client/Whiteboard/src/Components/Whiteboard.tsx
+++ Client/Whiteboard/src/Components/Whiteboard.tsx
@@ -1,6 +1,25 @@
-
+import React , { useEffect,useRef } from "react"
+import fabric from "fabric"
+import { io } from "socket.io-client" 
 
 function Whiteboard() {
+
+   const canvasRef = useRef<fabric.Canvas | null> (null);
+
+   useEffect(() => {
+        const canvas = new fabric.Canvas("canvas", {
+             isDrawingMode: true,
+            backgroundColor: "white",
+            width: window.innerWidth,
+             height: window.innerHeight,
+        })
+        canvasRef.current = canvas
+
+
+   })
+
+
+
   return (
     <div className="bg-amber-500">Whiteboard</div>
   )
