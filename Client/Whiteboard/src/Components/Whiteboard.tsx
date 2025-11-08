20ae22e04a5fafcfe7791bacbe9a200621769194
Krishna-zz <krishnagunjal022@gmail.com>
2025-11-08 09:47:04 +0530

    making modifications in the whiteboard

--- Client/Whiteboard/src/Components/Whiteboard.tsx
+++ Client/Whiteboard/src/Components/Whiteboard.tsx
@@ -8,20 +8,22 @@ const Whiteboard = () => {
 
   useEffect(() => {
     const canvasEl = canvasRef.current;
-    if (!canvasEl) return;
+    
 
     // Initialize canvas
     const canvas = new fabric.Canvas(canvasEl, {
       isDrawingMode: true,
       backgroundColor: "#ffffff",
+      
     });
 
     // Set brush settings
     const brush = canvas.freeDrawingBrush;
-    if (brush) {
+   
       brush.width = 5;
       brush.color = "black";
-    }
+      
+    
 
     // Render and cleanup
     canvas.renderAll();
@@ -35,13 +37,19 @@ const Whiteboard = () => {
     <div
       className="flex items-center justify-center h-[100vh] bg-gray-400 "
     >
+      <div className="bg-white rounded-2xl shadow hover:shadow-2xl hover:shadow-blue-400 p-2">
       <canvas
         ref={canvasRef}
         width={800}
         height={600}
         id="canvas"
-        className="bg-white rounded-2xl shadow hover:shadow-2xl hover: shadow-blue-400"
+        style={{
+        display: "block",
+        background: "white",
+        borderRadius: "1rem", // optional safe rounding
+      }}
       />
+      </div>
     </div>
   );
 }
